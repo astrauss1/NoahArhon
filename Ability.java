3034624ad386ce9b43084a9e27940ac1726e488e
Airhorn101 <natsufiredragonslayer101@gmail.com>
2019-07-02 16:52:08 -0700

    did some Ability

--- Ability.java
+++ Ability.java
@@ -1,1 +1,18 @@
-public class Ability {}
+public class Ability {
+    private int damage;
+    private double dmgMult;
+    private double dmgRed;
+    private String name;
+    public Ability(String name, int damage, double dmgMult, double dmgRed){
+        this.name=name;
+        this.damage=damage;
+        this.dmgMult=dmgMult;
+        this.dmgRed=dmgRed;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public int getMultdmg(int damage){
+        double tmp=this.dmgMult;
+    }
+}
