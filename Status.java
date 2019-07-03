8c19a751e1b362010ccce800f56ef8aec1b103b4
Airhorn101 <natsufiredragonslayer101@gmail.com>
2019-07-03 13:53:55 -0700

    added status class

---
+++ Status.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+public class Status{
+    private String name;
+    public Status(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return this.name;
+    }
+}
