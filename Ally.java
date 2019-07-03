74feac5ac7e76350a75b8cdf6fe4fe47e368b2f5
Airhorn101 <natsufiredragonslayer101@gmail.com>
2019-07-03 13:48:37 -0700

    add ally class

---
+++ Ally.java
@@ -0,0 +1,24 @@
+public class Ally extends Entity {
+    private Weapon basicAttack;
+    private Armor[] armor = {
+        new Armor("None", 0, 0, 0), 
+        new Armor("None", 0, 0, 0), 
+        new Armor("None", 0, 0, 0), 
+        new Armor("None", 0, 0, 0)
+    };
+
+    public Ally (int maxHealth, String name, Weapon basicAttack, Armor[] armor) {
+        super(maxHealth, name);
+        this.basicAttack = basicAttack;
+        this.armor = armor;
+    }
+
+    public Ally(int maxHealth, int level, String name, Weapon basicAttack) {
+        super(maxHealth, name);
+        this.basicAttack = basicAttack;
+    }
+
+    public Weapon getBasicAttack() {
+        return this.basicAttack;
+    }
+}
