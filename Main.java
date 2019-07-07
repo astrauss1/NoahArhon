8490f24d70e6395bab32854b94c5d352d5b45c52
aprzn123 <38828335+aprzn123@users.noreply.github.com>
2019-07-07 01:15:03 -0400

    Started writing javadocs

--- Main.java
+++ Main.java
@@ -8,14 +8,19 @@ import game.mgmt.Combat;
 import game.mgmt.Location;
 import game.entities.Entity;
 
+/**
+ * Just the Main class, runs everything.
+ */
 public class Main {
     public static void main(String[] args) {
         Player b = new Player(50, 1, "hi", new int[]{0, 0}, new Weapon("asdf", 10), 10);
-        Enemy a = new Enemy(50, "a", new int[]{2, 4}, new Weapon("stuff", 10));
+        System.out.println(b.getMaxHealth());
+        Enemy a = new Enemy(50, "a", new int[]{2, 4}, new Weapon("stuff", 5));
         Location test = new Location("test", 20, 6);
         Combat doCombat = new Combat();
 
         Scanner userMove = new Scanner(System.in);
+        // uM: the letter the user inputs, be it w, a, s, or d.
         String uM;
         while(true){
             test.drawRoom(b, a);
--- game/entities/Entity.java
+++ game/entities/Entity.java
@@ -1,49 +1,75 @@
 package game.entities;
 
+/**
+ * The Entity class includes all enemies, allies, bosses, and the player.
+ */
 public class Entity {
     private int health;
     private int maxHealth;
     private String name;
-    private int MoveSpeed;
     private int[] location;
     private int[] prevLoc = new int[2];
     public Entity(int maxHealth, String name, int[] location) {
         this.maxHealth = maxHealth;
         this.health = maxHealth;
         this.name = name;
-        this.MoveSpeed=MoveSpeed;
         this.location = location;
     }
 
+    /**
+     * Get the name of the entity.
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Take a set amount of damage. 
+     * @param damage The amount of damage to take.
+     */
     public void takeDamage(int damage) {
         this.health -= damage;
     }
 
+    /**
+     * Gets the current health of the entity.
+     * @return The current health of the entity.
+     */
     public int getHealth() {
         return this.health;
     }
 
+    /**
+     * Gets the entity's max health.
+     * @return The max health of the entity.
+     */
     public int getMaxHealth() {
         return this.maxHealth;
     }
     
+    /**
+     * Heals a certain amount of damage.
+     * @param damage The amount of damage to heal.
+     */
     public void heal(int damage) {
         this.health += damage;
         if (this.health > this.maxHealth) {
             this.health = this.maxHealth;
         }
     }
-    public int getMove(char m){
-        return this.MoveSpeed;
-    }
 
+    /**
+     * Gets the location of the entity.
+     * @return Location of the enitity.
+     */
     public int[] getLocation() {
         return this.location;
     }
+
+    /**
+     * Moves the entity to a different location.
+     * @param dir a character w, a, s, or d which refers to which direction to move the character. 
+     */
     public void move(String dir) {
         prevLoc[0] = location[0];
         prevLoc[1] = location[1];
@@ -63,11 +89,27 @@ public class Entity {
         }
     }
     
+    /**
+     * Moves an entity back to its previous space. 
+     * Useful, for instance, in situations where after moving
+     * there might be a collision causing you to have to move back.
+     */
     public void moveBack() {
+        /* We can't just use
+         * this.location = this.prevLoc; 
+           because then they would both point to 
+           the same array so moveBack would do 
+           nothing after its first usage */
         this.location[0] = this.prevLoc[0];
         this.location[1] = this.prevLoc[1];
     }
 
+    /**
+     * Checks if two entities are in the same location. 
+     * @param a Any entity to compare the location of.
+     * @param b Any to compare the location of.
+     * @return Whether the entities are in the same location.
+     */
     public static boolean sameLocation(Entity a, Entity b) {
         return (a.getLocation()[0] == b.getLocation()[0] && a.getLocation()[1] == b.getLocation()[1]);
     }
